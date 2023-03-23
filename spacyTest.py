b6cc8bcbf1510700c2cfad7c6a63bce18c340b26
danoprica <danutzoprica@yahoo.com>
2023-03-23 11:55:03 +0200

    Lemmas and removed duplicates for uniques

--- spacyTest.py
+++ spacyTest.py
@@ -2,17 +2,31 @@ import spacy
 import pandas as pd
 nlp = spacy.load('en_core_web_lg')
 df_uniques_dick = pd.read_csv("works/dick/dick_uniquesSupersense.tsv", delimiter="\t")
-#for row in df_uniques_dick['text']:
-    #text = ''
-    #for token in nlp(row):
-        #text = text + token
-    #print (text)  
-        
 
-test = 'thinking known unknown went'
-test = nlp(test)
-y = " ".join([token.lemma_ for token in test]) 
-print(y)
+for i, row in df_uniques_dick.iterrows():
+    #text = nlp(row[i, 'text'])
+    
+    #df_uniques_dick.at[i, 'text'] = text
+    text = row['text']
+    text = nlp(text)
+    text = "".join([token.lemma_ for token in text])
+    row['text'] = text
+
+df_uniques_dick = df_uniques_dick.drop_duplicates()
+print(df_uniques_dick)
+
+#for row in df_uniques_dick['text']:
+    #text = nlp(row)
+    #text = " ".join([token.lemma_ for token in text])  
+    #print (text)
+
+
+#print (df_uniques_dick)        
+
+#test = 'thinking known unknown went'
+#test = nlp(test)
+#y = " ".join([token.lemma_ for token in test]) 
+#print(y)
 
 #df_uniques_dick['text'].apply(lambda row: " ".join([w.lemma_ for w in nlp(row)]))
 
